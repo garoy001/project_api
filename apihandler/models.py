8cef1da7c156cea5320f73d95b94a5c8a312ff2f
Gabriel Royce <geroyceus@gmail.com>
2023-02-26 19:12:33 -0500

    fixed img url

--- apihandler/models.py
+++ apihandler/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    img = models.CharField(max_length=100)
+    img = models.CharField(max_length=500)
     gitlink = models.CharField(max_length=100)
     livelink = models.CharField(max_length=100)
